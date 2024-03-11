55d7e767fb93f28422372dc4f5e61e8a2b160eb6
TheOne_Neo <nemohap022@pop-os.localdomain>
2024-03-11 13:01:07 +0200

    . . .

--- functionality/calculations.js
+++ functionality/calculations.js
@@ -1,11 +1,40 @@
 // create class to store information of what input is inserted on calculator
-class calculator{
+class Calculator{
     constructor(firstOperant, secondperant){
         this.firstOperant = firstOperant
         this.secondperant = secondperant
+        this.clearButton()  //set all inputs to defualt ,empty when calc starts
     }
-}
 
+
+
+clearButton(){
+    this.first = ""
+    this.second = ''
+    this.operation = undefined
+    }
+
+deleteButton(){
+
+    }
+
+updateDisplay(){
+    this.firstOperant.innerText = this.first
+    }
+
+result(){// display calc 
+
+    }
+
+appendNumber(number){ //appends values selectted
+    this.first =number
+    }
+
+chooseOperation(operand){//select operands
+
+    }
+
+}
 
 const numberButtons = document.querySelectorAll('[data-numbers]')           //get access to data attributes specifically calculator numbers in html
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -15,27 +44,8 @@ const clearButton = document.querySelector('[data-all-clear]')
 const firstOperant = document.querySelector('[data-first-input-value]')
 const secondperant = document.querySelector('[data-second-input-value]')
 
+const calculator = new Calculator(firstOperant, secondperant )
 
-clearButton(){
-
-}
-
-deleteButton(){
-
-}
-
-updateDisplay(){
-    
-}
-
-result(){// display calc 
-
-}
-
-appendNumber(number){ //appends values selectted
-
-}
-
-chooseOperation(operand){//select operands
-
-}
+numberButtons.forEach(button=>{button.addEventListener('click',()=>{calculator.appendNumber(button.innerText)
+calculator.updateDisplay()})
+})
